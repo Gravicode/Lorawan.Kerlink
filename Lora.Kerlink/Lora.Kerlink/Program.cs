242ab8cd781050516bc584cfea17fe96e8cb3d10
mifmasterz <mifmasterz@yahoo.com>
2018-04-12 13:59:52 +0700

    lorawan kerlink

---
+++ Lora.Kerlink/Lora.Kerlink/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+using System.Text;
+using System.IO.Ports;
+using Microsoft.SPOT.Hardware;
+
+namespace Lora.Kerlink
+{
+    public partial class Program
+    {
+        SerialPort UART = null;
+
+        void PrintToLcd(string Message)
+        {
+            characterDisplay.Clear();
+            characterDisplay.Print(Message);
+        }
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            characterDisplay.BacklightEnabled = true;
+            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
+            Debug.Print("Program Started");
+            UART = new SerialPort(GHI.Pins.FEZSpiderII.Socket11.SerialPortName, 57600);
+            UART.ReadTimeout = 0;
+            Debug.Print("57600");
+            Debug.Print("RN2483 Test");
+            PrintToLcd("RN2483 Test");
+            OutputPort reset = new OutputPort(GHI.Pins.FEZSpiderII.Socket11.Pin6, false);
+            OutputPort reset2 = new OutputPort(GHI.Pins.FEZSpiderII.Socket11.Pin3, false);
+
+            reset.Write(true);
+            reset2.Write(true);
+
+            Thread.Sleep(50);
+            reset.Write(false);
+            reset2.Write(false);
+
+            Thread.Sleep(50);
+            reset.Write(true);
+            reset2.Write(true);
+
+            Thread.Sleep(50);
+
+            waitForResponse();
+
+            sendCmd("sys factoryRESET");
+            sendCmd("sys get hweui");
+            sendCmd("mac get deveui");
+
+            // For TTN
+            sendCmd("mac set devaddr AABBCCDD");  // Set own address
+            sendCmd("mac set appskey 2B7E151628AED2A6ABF7158809CF4F3C");
+            sendCmd("mac set nwkskey 2B7E151628AED2A6ABF7158809CF4F3C");
+            sendCmd("mac set adr off");
+            sendCmd("mac set rx2 3 868400000");//869525000
+            sendCmd("mac join abp");
+            sendCmd("mac get status");
+            sendCmd("mac get devaddr");
+            Thread.Sleep(1000);
+            Thread th1 = new Thread(new ThreadStart(Loop));
+            th1.Start();
+        }
+
+        void Loop()
+        {
+            characterDisplay.Clear();
+            characterDisplay.Print("Ready");
+            int counter = 0;
+            while (true)
+            {
+                counter++;
+                var data = new SensorData()
+                {
+                    Temp = tempHumidSI70.TakeMeasurement().Temperature,
+                    Humid = tempHumidSI70.TakeMeasurement().RelativeHumidity,
+                    Light = lightSense.GetIlluminance()
+                };
+                var jsonStr = Json.NETMF.JsonSerializer.SerializeObject(data);
+                Debug.Print("kirim :" +jsonStr);
+                PrintToLcd("send count: " + counter);
+                sendData(jsonStr);
+                Thread.Sleep(5000);
+                byte[] rx_data = new byte[20];
+
+                if (UART.CanRead)
+                {
+                    var count = UART.Read(rx_data, 0, rx_data.Length);
+                    if (count > 0)
+                    {
+                        Debug.Print("count:" + count);
+                        var hasil = new string(System.Text.Encoding.UTF8.GetChars(rx_data));
+                        Debug.Print("read:" + hasil);
+                        characterDisplay.Clear();
+                        characterDisplay.Print(hasil);
+
+                    }
+                }
+                Thread.Sleep(5000);
+            }
+        }
+
+        void sendCmd(string cmd)
+        {
+            byte[] rx_data = new byte[20];
+            Debug.Print(cmd);
+            Debug.Print("\n");
+            // flush all data
+            UART.Flush();
+            // send some data
+            var tx_data = Encoding.UTF8.GetBytes(cmd);
+            UART.Write(tx_data, 0, tx_data.Length);
+            tx_data = Encoding.UTF8.GetBytes("\r\n");
+            UART.Write(tx_data, 0, tx_data.Length);
+            Thread.Sleep(100);
+            while (!UART.IsOpen)
+            {
+                UART.Open();
+                Thread.Sleep(100);
+            }
+            if (UART.CanRead)
+            {
+                var count = UART.Read(rx_data, 0, rx_data.Length);
+                if (count > 0)
+                {
+                    Debug.Print("count:" + count);
+                    var hasil = new string(System.Text.Encoding.UTF8.GetChars(rx_data));
+                    Debug.Print("read:" + hasil);
+                }
+            }
+        }
+
+        void waitForResponse()
+        {
+            byte[] rx_data = new byte[20];
+
+            while (!UART.IsOpen)
+            {
+                UART.Open();
+                Thread.Sleep(100);
+            }
+            if (UART.CanRead)
+            {
+                var count = UART.Read(rx_data, 0, rx_data.Length);
+                if (count > 0)
+                {
+                    Debug.Print("count:" + count);
+                    var hasil = new string(System.Text.Encoding.UTF8.GetChars(rx_data));
+                    Debug.Print("read:" + hasil);
+                }
+
+            }
+        }
+
+        char getHexHi(char ch)
+        {
+            int nibbleInt = ch >> 4;
+            char nibble = (char)nibbleInt;
+            int res = (nibble > 9) ? nibble + 'A' - 10 : nibble + '0';
+            return (char)res;
+        }
+        char getHexLo(char ch)
+        {
+            int nibbleInt = ch & 0x0f;
+            char nibble = (char)nibbleInt;
+            int res = (nibble > 9) ? nibble + 'A' - 10 : nibble + '0';
+            return (char)res;
+        }
+
+        void sendData(string msg)
+        {
+            byte[] rx_data = new byte[20];
+            char[] data = msg.ToCharArray();
+            Debug.Print("mac tx uncnf 1 ");
+            var tx_data = Encoding.UTF8.GetBytes("mac tx uncnf 1 ");
+            UART.Write(tx_data, 0, tx_data.Length);
+
+            // Write data as hex characters
+            foreach (char ptr in data)
+            {
+                tx_data = Encoding.UTF8.GetBytes(new string(new char[] { getHexHi(ptr) }));
+                UART.Write(tx_data, 0, tx_data.Length);
+                tx_data = Encoding.UTF8.GetBytes(new string(new char[] { getHexLo(ptr) }));
+                UART.Write(tx_data, 0, tx_data.Length);
+
+
+                Debug.Print(new string(new char[] { getHexHi(ptr) }));
+                Debug.Print(new string(new char[] { getHexLo(ptr) }));
+            }
+            tx_data = Encoding.UTF8.GetBytes("\r\n");
+            UART.Write(tx_data, 0, tx_data.Length);
+            Debug.Print("\n");
+            Thread.Sleep(5000);
+
+            if (UART.CanRead)
+            {
+                var count = UART.Read(rx_data, 0, rx_data.Length);
+                if (count > 0)
+                {
+                    Debug.Print("count:" + count);
+                    var hasil = new string(System.Text.Encoding.UTF8.GetChars(rx_data));
+                    Debug.Print("read:" + hasil);
+                }
+            }
+        }
+    }
+
+    public class SensorData
+    {
+        public double Humid { get; set; }
+        public double Light { get; set; }
+        public double Temp { get; set; }
+    }
+}
---
+++ Lora.Kerlink/Lora.UdpReceiver/Program.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lora.UdpReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Start receiving UDP from Kerlink gateway..");
+            Task loop = new Task(new Action(Loop));
+            loop.Start();
+            Console.ReadLine();
+        }
+
+        static void Loop()
+        {
+            UdpClient udpServer = new UdpClient(8888);
+
+            while (true)
+            {
+                var remoteEP = new IPEndPoint(IPAddress.Any, 8888);
+                var data = udpServer.Receive(ref remoteEP); // listen on port 8888
+
+                var datastr = System.Text.Encoding.Default.GetString(data);
+                Console.WriteLine("receive data from " + remoteEP.ToString());
+                Console.WriteLine("data: " + datastr);
+                var obj = JsonConvert.DeserializeObject<RootObject>(datastr);
+                if (obj != null)
+                {
+                    byte[] databyte = Convert.FromBase64String(obj.rx.userdata.payload);
+                    string decodedString = Encoding.UTF8.GetString(databyte);
+                    var originalValue = Unpack(decodedString);
+                    Console.WriteLine("unpack :" + originalValue);
+                    var sensorValue = JsonConvert.DeserializeObject<SensorData>(originalValue);
+                    sensorValue.Tanggal = DateTime.Now;
+                    //call power bi api
+                    SendToPowerBI(sensorValue);
+                    Thread.Sleep(5000);
+                }
+               
+
+                
+            }
+         
+
+        }
+        private static HttpClient _client;
+
+        public static HttpClient client
+        {
+            get
+            {
+                if (_client == null) _client = new HttpClient();
+                return _client;
+            }
+
+        }
+        
+      
+        static async void SendToPowerBI(SensorData data)
+        {
+            var url = "https://api.powerbi.com/beta/e4a5cd36-e58f-4f98-8a1a-7a8e545fc65a/datasets/c3152879-fd74-4ba6-94aa-2b9b7111005f/rows?key=AUL%2FVTsGwsmJGzP28v7ah5EInDrjg7rTXx4b1IarBiTTcuB62zXzkG8QGoCZuJwyICzydqAT6ieTzGxsMXMETQ%3D%3D";
+            var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+            var res = await client.PostAsync(url, content, CancellationToken.None);
+            if (res.IsSuccessStatusCode)
+            {
+                Console.WriteLine("data sent to power bi - " + DateTime.Now);
+            }
+        }
+        public static int FromHex(char digit)
+        {
+
+            if ('0' <= digit && digit <= '9')
+            {
+
+                return (int)(digit - '0');
+
+            }
+
+
+
+            if ('a' <= digit && digit <= 'f')
+
+                return (int)(digit - 'a' + 10);
+
+
+
+            if ('A' <= digit && digit <= 'F')
+
+                return (int)(digit - 'A' + 10);
+
+
+
+            throw new ArgumentException("digit");
+
+        }
+
+
+
+        public static string Unpack(string input)
+        {
+
+            byte[] b = new byte[input.Length / 2];
+
+
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+
+                b[i / 2] = (byte)((FromHex(input[i]) << 4) | FromHex(input[i + 1]));
+
+            }
+
+            return new string(Encoding.UTF8.GetChars(b));
+
+        }
+
+
+    }
+    public class Motetx
+    {
+        public int freq { get; set; }
+        public string modu { get; set; }
+        public string datr { get; set; }
+        public string codr { get; set; }
+    }
+
+    public class Userdata
+    {
+        public int seqno { get; set; }
+        public int port { get; set; }
+        public string payload { get; set; }
+        public Motetx motetx { get; set; }
+    }
+
+    public class Gwrx
+    {
+        public string time { get; set; }
+        public int chan { get; set; }
+        public int rfch { get; set; }
+        public int rssi { get; set; }
+        public double lsnr { get; set; }
+    }
+
+    public class Rx
+    {
+        public string moteeui { get; set; }
+        public Userdata userdata { get; set; }
+        public List<Gwrx> gwrx { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Rx rx { get; set; }
+    }
+
+      public class SensorData
+    {
+          public DateTime Tanggal { get; set; }
+        public double Humid { get; set; }
+        public double Light { get; set; }
+        public double Temp { get; set; }
+      }
+}
