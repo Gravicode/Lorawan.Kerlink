7ad67095d4383cc5cf1663c7a47c630dae2eef8c
mif <mifmasterz@yahoo.com>
2018-06-13 14:00:15 +0700

    add http transceiver

---
+++ Lora.Kerlink/Lora.HttpTransceiver/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Lora.HttpTransceiver
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+            appBuilder.UseWebApi(config);
+        }
+    }
+    public class BrowserJsonFormatter : JsonMediaTypeFormatter
+    {
+        public BrowserJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            this.SerializerSettings.Formatting = Formatting.Indented;
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+}
---
+++ Lora.Kerlink/Lora.HttpTransceiver/ValuesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Lora.HttpTransceiver
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public List<Model.Transmitter.TransmitModel> Get()
+        {
+            var newResp = new List<Model.Transmitter.TransmitModel>();
+            var newNode = new Model.Transmitter.TransmitModel();
+            newNode.tx = new Model.Transmitter.Tx();
+            newNode.tx.moteeui = "AAABBBEE";
+            newNode.tx.trycount = 5;
+            newNode.tx.txmsgid = "000000000001";
+            newNode.tx.txsynch = false;
+            newNode.tx.userdata = new Model.Transmitter.Userdata();
+            newNode.tx.userdata.payload = "Njg2NTZjNmM2ZjIwNjM2ZjZkNzA3NTc0NjU3Mg==";
+            newNode.tx.userdata.port = 2;
+            newResp.Add(newNode);
+            return newResp;
+            
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public List<Model.Transmitter.TransmitModel> Post([FromBody]List<Model.Receiver.ReceiveModel> value)
+        {
+            foreach (var item in value)
+            {
+                Console.WriteLine("from kerlink:" + item.rx.userdata.seqno + ":" + item.rx.userdata.payload);
+
+            }
+            var newResp = new List<Model.Transmitter.TransmitModel>();
+            var newNode = new Model.Transmitter.TransmitModel();
+            newNode.tx = new Model.Transmitter.Tx();
+            newNode.tx.moteeui = "AAABBBEE";
+            newNode.tx.trycount = 5;
+            newNode.tx.txmsgid = "000000000001";
+            newNode.tx.txsynch = false;
+            newNode.tx.userdata = new Model.Transmitter.Userdata();
+            newNode.tx.userdata.payload = "Njg2NTZjNmM2ZjIwNjM2ZjZkNzA3NTc0NjU3Mg==";
+            newNode.tx.userdata.port = 2;
+            newResp.Add(newNode);
+            return newResp;
+            
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    } 
+
+}
+
+namespace Model.Transmitter
+{
+    public class Userdata
+    {
+        public int port { get; set; }
+        public string payload { get; set; }
+    }
+
+    public class Tx
+    {
+        public string moteeui { get; set; }
+        public string txmsgid { get; set; }
+        public int trycount { get; set; }
+        public bool txsynch { get; set; }
+        public Userdata userdata { get; set; }
+    }
+
+    public class TransmitModel
+    {
+        public Tx tx { get; set; }
+    }
+}
+
+namespace Model.Receiver
+{
+    public class Motetx
+    {
+        public int freq { get; set; }
+        public string modu { get; set; }
+        public string datr { get; set; }
+        public string codr { get; set; }
+    }
+
+    public class Userdata
+    {
+        public int seqno { get; set; }
+        public int port { get; set; }
+        public string payload { get; set; }
+        public Motetx motetx { get; set; }
+    }
+
+    public class Gwrx
+    {
+        public string time { get; set; }
+        public int chan { get; set; }
+        public int rfch { get; set; }
+        public int rssi { get; set; }
+        public double lsnr { get; set; }
+    }
+
+    public class Rx
+    {
+        public string moteeui { get; set; }
+        public Userdata userdata { get; set; }
+        public List<Gwrx> gwrx { get; set; }
+    }
+
+    public class ReceiveModel
+    {
+        public Rx rx { get; set; }
+    }
+}
